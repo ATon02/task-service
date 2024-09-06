cf3de43a808ac12311627d508f59ba203f51e668
Anderson Arley Tonusco Ruano <anderson.tonusco@vivetu.com.co>
2024-09-06 18:55:02 -0500

    Agrega header para validacion al momento de comunicarse entre servicios

--- src/main/java/com/example/taskservice/intercom/UserIntercom.java
+++ src/main/java/com/example/taskservice/intercom/UserIntercom.java
@@ -1,6 +1,8 @@
 package com.example.taskservice.intercom;
 
 import com.example.taskservice.intercom.models.DTOUserEntityResponse;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -9,11 +11,14 @@ import reactor.core.publisher.Mono;
 @Service
 public class UserIntercom {
 
-    private WebClient webClient = WebClient.builder().baseUrl("http://localhost:8081/api/users").build();
+    @Value("${header.internal}")
+    private String internal;
+
+    private WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080/api/users").build();
 
     public Mono<DTOUserEntityResponse> verifyUserExist(Long userId) {
         return webClient.get()
-                .uri("/" + userId)
+                .uri("/" + userId).header("internal", internal)
                 .retrieve()
                 .bodyToMono(DTOUserEntityResponse.class)
                 .onErrorResume(e -> {
